9ee7857819d53c78a84e094404b402738d332c08
Ramon <ramoncasadofranco@hotmail.es>
2024-11-16 19:36:53 +0100

    First push

---
+++ src/main/java/modelo/bean/LoginBean.java
@@ -0,0 +1,49 @@
+package modelo.bean;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+
+@Named("login")
+@SessionScoped
+public class LoginBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String nombre;
+	private String password;
+	private Date fecha;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	public String comprobar() {
+		if (nombre.equals("pirata")) {
+			return "error";
+		} else {
+			return "ok";
+		}
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+}
