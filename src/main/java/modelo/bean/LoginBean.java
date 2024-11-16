6fbcb755ae581f32bd49f7762da33fe2043a0dd0
Ramon <ramoncasadofranco@hotmail.es>
2024-11-16 20:49:21 +0100

    Laboratorio jsf 2

--- src/main/java/modelo/bean/LoginBean.java
+++ src/main/java/modelo/bean/LoginBean.java
@@ -3,7 +3,11 @@ package modelo.bean;
 import java.io.Serializable;
 import java.util.Date;
 
+import org.primefaces.event.SelectEvent;
+
 import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
 
 @Named("login")
@@ -32,6 +36,12 @@ public class LoginBean implements Serializable {
 
 	
 	public String comprobar() {
+		if(nombre.length()!=password.length()) {
+			FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(
+					"Error: La longitud del nombre y de la contraseña son diferentes."));
+			return null;
+		}
+		
 		if (nombre.equals("pirata")) {
 			return "error";
 		} else {
@@ -45,5 +55,10 @@ public class LoginBean implements Serializable {
 
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
+	}
+	
+	public void onDateSelect(SelectEvent event) {
+		FacesContext.getCurrentInstance().addMessage(null,
+		new FacesMessage("Fecha escogida: "+event.getObject()));
 	}
 }
